1c1292210f14c1126cd3e3d533c8d188dcfa7050
hristijankostadinoski <hristijan@2coders.com>
2024-04-22 20:57:34 +0200

    lecture 0: console.log, comments and data types in JS

---
+++ lecture_0/index.js
@@ -0,0 +1,36 @@
+// === Getting started with JavaScript ====
+console.log("Hristijan Kostadinoski")
+
+// ova e komentar
+
+// Data Types
+
+// Primitive data types:
+var myNumber = 100 // number
+var myName = "Hristijan" // string
+var myBool = true // boolean
+var myUndefined = undefined // undefined
+var myNullType = null // null
+
+// Referance data types:
+
+// Object
+var myObject = { car: "Mercedes", model: "Benz" }
+console.log(myObject.model)
+
+// Array
+var myArray = [1, true, "random string", "o", null, [], {}, undefined]
+console.log(myArray)
+
+// var, let, const
+var myVariable = 10
+
+let age = 30 // declaration of a variable
+console.log(age)
+
+age = 34 // changing the value of the 'age' variable
+
+console.log(age) // 34
+
+const PI = 3.14
+// console.log(PI)
