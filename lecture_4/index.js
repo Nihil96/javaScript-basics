bd316cec952aee092a14b49c0799aa1127dabc2f
hristijankostadinoski <hristijan@2coders.com>
2024-05-20 21:38:27 +0200

    async/await and try/catch - examples

--- lecture_4/index.js
+++ lecture_4/index.js
@@ -58,3 +58,52 @@ const myPromise = new Promise((resolve, reject) => {
 myPromise
   .then((result) => console.log(result)) // Output: Success!
   .catch((error) => console.log(error))
+
+// Chaning Promises
+const promise1 = new Promise((resolve) => resolve(1))
+
+promise1
+  .then((number) => number + 1)
+  .then((prevNumber) => prevNumber + 1)
+  .then((result) => console.log(result))
+
+// Async/Await in JavaScript
+
+function getUserData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const userData = { name: "Mirko", surname: "Petkovski", age: 32 }
+      const isUserAvailable = false
+      if (isUserAvailable) {
+        resolve(userData)
+      } else {
+        reject("ERROR: The user is not found")
+      }
+    }, 3000)
+  })
+}
+
+// one way of resolving the Promise
+// getUserData()
+//   .then((user) => console.log(user))
+//   .catch((error) => console.log(error))
+
+// using async/await
+async function fetchUserData() {
+  const user = await getUserData()
+  console.log(user)
+}
+
+// fetchUserData()
+
+// try/catch blocks - second way of handling Promises
+async function fetchUser() {
+  try {
+    const user = await getUserData()
+    console.log(user)
+  } catch (error) {
+    console.log("Something went wrong!", error)
+  }
+}
+
+fetchUser()
